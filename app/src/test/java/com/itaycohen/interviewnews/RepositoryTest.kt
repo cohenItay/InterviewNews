91156d90ee17642bcc5f62b70405227ef1f5ad39
itay cohen <itay-co@yit.co.il>
2020-04-07 01:56:52 +0300

    repo unit test

---
+++ app/src/test/java/com/itaycohen/interviewnews/RepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.itaycohen.interviewnews
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.itaycohen.interviewnews.data_layer.articles.ArticlesRepository
+import com.itaycohen.interviewnews.data_layer.articles.ArticlesWebService
+import com.itaycohen.interviewnews.data_layer.articles.models.TopHeadlinesModel
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import retrofit2.Response
+
+
+class RepositoryTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule() // for livedata looper
+
+    @Test
+    fun whenUpdateHeadlineSuccess_LiveDataIsUpdated() {
+        val topHeadlinesMock = mock(TopHeadlinesModel::class.java)
+        `when`(topHeadlinesMock.status).thenReturn("OK")
+        val webService = mock<ArticlesWebService> {
+            onBlocking { getHeadlines("us") } doReturn Response.success(topHeadlinesMock)
+        }
+        val repo = ArticlesRepository(webService)
+        runBlocking {
+            repo.updateTopHeadlines("us")
+        }
+        assertEquals("OK", repo.topHeadlinesLiveData.value!!.status)
+    }
+}
