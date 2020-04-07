b2561c565b43c4e90801c1063adb8c97a8cda3b9
itay cohen <itay-co@yit.co.il>
2020-04-07 15:23:28 +0300

    add idle resource

---
+++ app/src/androidTest/java/com/itaycohen/interviewnews/NavigateToDetailedFragmentTest.kt
@@ -0,0 +1,59 @@
+package com.itaycohen.interviewnews
+
+import androidx.fragment.app.testing.*
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.itaycohen.interviewnews.application.di.DaggerAppComponent
+import com.itaycohen.interviewnews.ui_layer.articles.ArticlesViewModel
+import com.itaycohen.interviewnews.ui_layer.articles.screen_list.ArticleListFragment
+
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * This Test will wait until data has loaded from web, click the first article
+ * and check that the Detailed fragment is visible.
+ */
+@RunWith(AndroidJUnit4::class)
+class NavigateToDetailedFragmentTest {
+
+    private lateinit var mIdleResource: IdlingResource
+
+    @Before
+    fun initNavTest() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val navController = TestNavHostController(context)
+        navController.setGraph(R.navigation.main_nav_graph)
+
+        val fragmentFactory = DaggerAppComponent.factory()
+            .create(context)
+            .getArticlesComponent()
+            .create()
+            .getFragmentFactory()
+
+        val fragScenario = launchFragmentInContainer<ArticleListFragment>(factory = fragmentFactory)
+        fragScenario.moveToState(Lifecycle.State.CREATED)
+        fragScenario.onFragment { listFrag ->
+            Navigation.setViewNavController(listFrag.requireView(), navController)
+            val sharedViewModel = ViewModelProvider(listFrag.requireActivity()).get(ArticlesViewModel::class.java)
+            IdlingRegistry.getInstance().register(sharedViewModel.idleResource)
+        }
+        fragScenario.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun startTest() {
+        assertEquals(true, true)
+    }
+}
--- app/src/main/java/com/itaycohen/interviewnews/ui_layer/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/itaycohen/interviewnews/ui_layer/articles/ArticlesViewModel.kt
@@ -1,8 +1,11 @@
 package com.itaycohen.interviewnews.ui_layer.articles
 
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.*
+import androidx.test.espresso.IdlingResource
 import com.itaycohen.interviewnews.data_layer.articles.ArticlesRepository
 import com.itaycohen.interviewnews.data_layer.network.QueryState
+import com.itaycohen.interviewnews.ui_layer.articles.idling_resources.SimpleIdleResource
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,13 +17,31 @@ class ArticlesViewModel @Inject constructor(
     private val repo: ArticlesRepository
 ) : ViewModel() {
 
+
     // each time repo live data is updated so does this live data
     val topHeadlinesLiveData = Transformations.map(repo.topHeadlinesLiveData) {it}
+
+    /**
+     * A delegate for checking if lazy is instantiated
+     */
+    private val idleResourceDelegate = lazy { SimpleIdleResource() }
+
+    /**
+    * Only called from test, creates and returns a new {@link SimpleIdlingResource}.
+    */
+    @VisibleForTesting
+    val idleResource: SimpleIdleResource by idleResourceDelegate
+
 
     /**
      * True if query is running
      */
     val queryStateLiveData: LiveData<QueryState> = MutableLiveData(QueryState.IDLE)
+
+
+    private val isEspressoTestRunning: Boolean
+        get() = idleResourceDelegate.isInitialized()
+
 
     fun updateTopHeadlines() {
         if (queryStateLiveData.value!!.state == QueryState.STATE_RUNNING)
@@ -30,11 +51,15 @@ class ArticlesViewModel @Inject constructor(
         viewModelScope.launch {
             mutableQueryLiveData.value = QueryState.LOADING
             try {
+                if (isEspressoTestRunning)
+                    idleResource.setIdle(false)
                 repo.updateTopHeadlines()
             } catch (error: Throwable) {
                 mutableQueryLiveData.value = QueryState.error(error)
             } finally {
                 mutableQueryLiveData.value = QueryState.LOADED
+                if (isEspressoTestRunning)
+                    idleResource.setIdle(true)
             }
         }
     }
